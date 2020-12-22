276bd55a7c0e2e36902bf6b9fb0d8c5c21ee3a84
ozanmujde <ozanmujde@googlemail.com>
2020-12-22 11:10:10 +0300

    small changes on import

--- src/main/java/pl/nogacz/checkers/application/StartMenu.java
+++ src/main/java/pl/nogacz/checkers/application/StartMenu.java
@@ -6,9 +6,15 @@ import pl.nogacz.checkers.board.Board;
 import java.awt.Color;
 
 import javax.sound.midi.Soundbank;
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowAdapter;
+
 import java.util.ArrayList;
 import java.io.File;
 
--- src/main/java/pl/nogacz/checkers/board/Board.java
+++ src/main/java/pl/nogacz/checkers/board/Board.java
@@ -20,9 +20,11 @@ import pl.nogacz.checkers.pawns.PawnClass;
 import pl.nogacz.checkers.pawns.PawnColor;
 import pl.nogacz.checkers.pawns.PawnMoves;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
-import java.awt.*;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
